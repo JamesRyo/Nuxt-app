a1ec1f7d6dffc503daaa7065d6e33c3cf4327e86
JamesRyo <ikekitamadoka@gmail.com>
2024-03-11 15:08:35 +0900

    fix:button change

--- pages/index.vue
+++ pages/index.vue
@@ -146,7 +146,7 @@ export default defineComponent({
         else{
           this.thumbsdownColor = 'gray';
         }
-          this.thumbsupColor = 'gray'; 
+          this.thumbsupColor = 'gray';
         }
       },
       toggleDarkMode() {
